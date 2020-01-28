e1901d0a5fec9672231744ff6f57a735653f9a2b
zhongming <zhongming139@126.com>
2020-01-28 21:24:38 +0800

    kfifo test

---
+++ mock_kfifo_nonblock_dev/test.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#define DEMO_DEV_NAME "/dev/my_demo_kfifo_nonblock_dev"
+
+int main()
+{
+	int fd;
+	int ret;
+
+	char testmessage[] = "Test Message for mock device!";
+
+	printf("test start");
+
+	fd = open(DEMO_DEV_NAME, O_RDWR);
+	if (fd < 0) {
+		printf("open device %s failed\n", DEMO_DEV_NAME);
+		return -1;
+	}
+	
+	int len = sizeof(testmessage);
+	ret = write(fd, testmessage, len);
+	if (ret != len) {
+		printf("Can't wirte on device %d, ret=%d", fd, ret);
+		return -1;
+	}
+	
+	
+
+	
+	char *read_buf = (char*) malloc(2 * len);
+	
+	ret = read(fd, read_buf, 2 * len);
+	close(fd);
+	
+	printf("Readed message size is %d, context is %s\n", ret, read_buf);
+
+	printf("test end\n");
+	
+	return 0;
+}
